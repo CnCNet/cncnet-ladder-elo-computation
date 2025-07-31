ef8d969e85d1f382be01f27c1ee97754118d45e2
Sneer-ra2 <forum@snear.de>
2025-07-31 02:42:58 +0200

    Yet another time issue fix.

--- main.cpp
+++ main.cpp
@@ -287,7 +287,7 @@ int main(int argc, char* argv[])
         Log::debug() << "Shifted time of game " << game->id() << " from "
                        << stringtools::fromDateTime(currentTimePoint) << " to " << stringtools::fromDateTime(shiftedTime) << ".";
 
-        if (gameDate > options.endDate)
+        if (gameDate >= options.endDate)
         {
             break;
         }
